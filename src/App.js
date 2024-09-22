74dd78bf5bcfdc5d71eb5333308818bb14e7685f
thandierab <thandierabasimane@gmail.com>
2024-09-23 00:48:26 +0200

    Added links to Github and Netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        <a
+          href="https://github.com/thandierab/react-weather-forecast"
+          target="blank"
+          title="Github"
+        >
+          Coded
+        </a>{" "}
+        by Thandie R-mane and hosted on{" "}
+        <a href="https://thandierab-reactforecast.netlify.app/" target="blank">
+          Netlify
+        </a>
+      </p>
+    </div>
+  );
 }
