02bb36b1f987febb0ee1e9c6e66c611d1ede0671
thandierab <thandierabasimane@gmail.com>
2024-09-26 06:03:27 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <p>
-        <a
-          href="https://github.com/thandierab/react-weather-forecast"
-          target="blank"
-          title="Github"
-        >
-          Coded
-        </a>{" "}
-        by Thandie R-mane and hosted on{" "}
-        <a href="https://thandierab-reactforecast.netlify.app/" target="blank">
-          Netlify
-        </a>
-      </p>
+      <div className="container">
+        <Weather />
+        <footer>
+          <a
+            href="https://github.com/thandierab/react-weather-forecast"
+            target="blank"
+            title="Github"
+          >
+            Coded
+          </a>{" "}
+          by Thandie R-mane and hosted on{" "}
+          <a
+            href="https://thandierab-reactforecast.netlify.app/"
+            target="blank"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
